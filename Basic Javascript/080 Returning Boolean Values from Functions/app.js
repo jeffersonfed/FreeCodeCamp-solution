de08dccd5be867f1b1cc8d0e9dcedb6e4ac3ed9a
Jefferson <stupidjeff430@gmail.com>
2023-07-20 17:41:00 +0900

    new file

--- Basic Javascript/080 Returning Boolean Values from Functions/app.js
+++ Basic Javascript/080 Returning Boolean Values from Functions/app.js
@@ -14,3 +14,20 @@
     // function isEqual(a, b) {
     //     return a === b;
     //   }
+
+    function isLess(a, b) {
+        // Only change code below this line
+        // if (a < b) {
+        //   return true;
+        // } else {
+        //   return false;
+        // }
+          return a <= b;
+          
+          
+        
+      
+        // Only change code above this line
+      }
+      
+      isLess(10, 15);
---
+++ Basic Javascript/081 Return Early Pattern for Functions/app.js
@@ -0,0 +1,12 @@
+//When a return statement is reached, 
+        //the execution of the current function stops and control returns to the calling location.
+
+        // function myFun() {
+        //     console.log("Hello");
+        //     return "World";
+        //     console.log("byebye")
+        //   }
+        //   myFun();
+
+        //The above will display the string Hello in the console, and return the string World. 
+        //The string byebye will never display in the console, because the function exits at the return statement.
