c46432f23cd3d61ba6d4332eb8bfa1fa0b19fd48
Jefferson <stupidjeff430@gmail.com>
2023-07-20 17:34:14 +0900

    update

--- Basic Javascript/080 Returning Boolean Values from Functions/app.js
+++ Basic Javascript/080 Returning Boolean Values from Functions/app.js
@@ -1,1 +1,16 @@
 // You may recall from 060 Comparison with the Equality Operator
+    //that all comparison operators return a boolean true or false value.
+
+    // function isEqual(a, b) {
+    //     if (a === b) {
+    //       return true;
+    //     } else {
+    //       return false;
+    //     }
+    //   }
+
+    // there's a better way to do this. Since === returns true or false, we can return the result of the comparison:
+
+    // function isEqual(a, b) {
+    //     return a === b;
+    //   }
