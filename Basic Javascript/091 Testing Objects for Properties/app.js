dd274930f59133762a6d2de2e3e6c03466330b8b
Jefferson <stupidjeff430@gmail.com>
2023-07-22 14:26:46 +0900

    update

--- Basic Javascript/091 Testing Objects for Properties/app.js
+++ Basic Javascript/091 Testing Objects for Properties/app.js
@@ -14,17 +14,15 @@ checkForProperty({ top: 'hat', bottom: 'pants' }, 'middle'); // false
 
 The first checkForProperty function call returns true, 
 while the second returns false.
+*/
+
 
 function checkObj(obj, checkProp) {
-  // Only change code below this line
-  if (obj.hasOwnProperty(checkProp)){
-      return obj[checkProp];
-  } else {
-    return "Not Found";
+    // Only change code below this line
+    if (obj.hasOwnProperty(checkProp)){
+        return obj[checkProp];
+    } else {
+      return "Not Found";
+    }
+    // Only change code above this line
   }
-  // Only change code above this line
-}
-
-
-
-*/
