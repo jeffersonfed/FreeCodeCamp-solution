58119a0c23a583c6853a9a57a38d6860ffa203c0
Jefferson <stupidjeff430@gmail.com>
2023-07-23 14:02:16 +0900

    add

--- Basic Javascript/101 Nesting For Loops/app.js
+++ Basic Javascript/101 Nesting For Loops/app.js
@@ -11,5 +11,22 @@ for (let i = 0; i < arr.length; i++) {
     console.log(arr[i][j]);
   }
 }
+This outputs each sub-element in arr one at a time. Note that for the inner loop, 
+we are checking the .length of arr[i], since arr[i] is itself an array.
 */
-s
+function multiplyAll(arr) {
+    let product = 1;
+    // Only change code below this line
+      for (let i = 0; i < arr.length; i++){
+        for(let j = 0; j < arr[i].length; j++){
+          product = product * arr[i][j];
+          console.log(product);
+        }
+      }
+    // Only change code above this line
+    return product;
+    
+  }
+  
+  multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+  
