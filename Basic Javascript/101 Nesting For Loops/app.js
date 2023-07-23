b4a7da5a29309baee75a5fb066ffa6b51abade8c
AI Kurumi <140081185+Codingifun@users.noreply.github.com>
2023-07-23 13:00:53 +0900

    Update app.js

---
+++ Basic Javascript/101 Nesting For Loops/app.js
@@ -0,0 +1,14 @@
+/*
+If you have a multi-dimensional array, you can use the same logic as the prior waypoint to loop through both the array and any sub-arrays. 
+
+Here is an example:
+const arr = [
+  [1, 2], [3, 4], [5, 6]
+];
+
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr[i].length; j++) {
+    console.log(arr[i][j]);
+  }
+}
+*/
