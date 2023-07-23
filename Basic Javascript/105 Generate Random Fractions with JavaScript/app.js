c5c8b4f27a9bf35790db119deef2e3e6cd15a08a
Jefferson <stupidjeff430@gmail.com>
2023-07-23 16:47:47 +0900

    add

---
+++ Basic Javascript/105 Generate Random Fractions with JavaScript/app.js
@@ -0,0 +1,20 @@
+/* 003 Storing Values with the Assignment Operator
+
+
+
+*/
+
+function randomFraction() {
+
+    // Only change code below this line
+      
+      var result = 0;
+      while (result === 0){
+        result = Math.random();
+      }
+  
+  
+    return result;
+  
+    // Only change code above this line
+  }
---
+++ Basic Javascript/106 Generate Random Whole Numbers with JavaScript/app.js
@@ -0,0 +1,6 @@
+function randomWholeNum() {
+
+
+    return Math.floor(Math.random() * 10);
+  }
+
