6e511f5e785235117363f57658997ffd2a80d23d
Jefferson <stupidjeff430@gmail.com>
2023-07-23 12:44:29 +0900

    add

--- Basic Javascript/096 Iterate with JavaScript While Loops/app.js
+++ Basic Javascript/096 Iterate with JavaScript While Loops/app.js
@@ -24,3 +24,5 @@ while (i >= 0){
   myArray.push(i);
   i--;
 }
+
+console.log(myArray);
---
+++ Basic Javascript/097 Iterate with JavaScript For Loops/app.js
@@ -0,0 +1,14 @@
+/*most common type of JavaScript loop is called a "for" loop 
+        because it runs for a specific number of times.
+
+        
+*/
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 1; i <= 5; i++){
+  myArray.push(i);
+}
+
+console.log(myArray);
