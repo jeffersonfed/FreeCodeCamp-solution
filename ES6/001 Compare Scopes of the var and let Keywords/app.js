7fdd91fe92c3909347a4ba1b05fc5d0553352c06
Jefferson <stupidjeff430@gmail.com>
2023-07-24 18:58:37 +0900

    add

---
+++ ES6/001 Compare Scopes of the var and let Keywords/app.js
@@ -0,0 +1,18 @@
+/*
+If you replace 'var' with 'let' in the code above, it results in an error:
+
+let camper = "James";
+let camper = "David";
+The error can be seen in your browser console.
+
+unlike 'var', when you use 'let', a variable with the same name can only be declared once.
+
+When you declare a variable with the 'var' keyword, it is declared globally, or locally if declared inside a function.
+
+
+The 'let' keyword behaves similarly, but with some extra features. 
+When you declare a variable with the 'let' keyword inside a block, statement, or expression, 
+its scope is limited to that block, statement, or expression.
+
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/es6/compare-scopes-of-the-var-and-let-keywords
+*/
